ca03f4fd49c0c6834d687dc09796a6634c21c5d1
Ahmed Waqar <uchihawa@gmail.com>
2023-11-22 23:34:28 +0500

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Kisi ko ghr sy nikalty hvy milgye manzil.....");
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+
+
+
+app.get('/')
